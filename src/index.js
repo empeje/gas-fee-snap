af236afab5ecfb2a06df6492359f5ecb89728fd4
Abdurrachman Mappuji <11813607+empeje@users.noreply.github.com>
2022-09-03 21:13:59 +0200

    Make gas fee price

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+async function getFees() {
+  const response = await fetch('https://www.etherchain.org/api/gasPriceOracle');
+  return response.text();
+}
+
 module.exports.onRpcRequest = async ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
+      // eslint-disable-next-line no-case-declarations
+      const fees = JSON.parse(await getFees());
+      const baseFee = fees.currentBaseFee;
+      const safeLow = Math.ceil(baseFee + parseFloat(fees.safeLow));
+      const standard = Math.ceil(baseFee + parseFloat(fees.standard));
+      const fastest = Math.ceil(baseFee + parseFloat(fees.fastest));
+
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${origin}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+            prompt: `Gas Fees`,
+            description: 'Current Gas Fees from etherchain.org:',
+            textAreaContent: `Base: ${baseFee} \n Low: ${safeLow} \n Average: ${standard} \n High: ${fastest}`,
           },
         ],
       });
